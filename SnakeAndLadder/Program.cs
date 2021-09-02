68ffeff2b31d360c6590573a17d5c669c01fcb48
ka1peshc <kalpesh111997@gmail.com>
2021-09-02 12:09:52 +0530

    Define constants

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -7,7 +7,9 @@ namespace SnakeAndLadder
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to snake ladder program.");
-            
+            //Constant 
+            int START = 0;
+            int POSITION = 0;
         }
     }
 }
