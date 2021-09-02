f06a3f9540936f55c9c31d72abeff3c1c223f235
ka1peshc <kalpesh111997@gmail.com>
2021-09-02 12:26:13 +0530

    Added noplay snake and ladder logic

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -9,11 +9,27 @@ namespace SnakeAndLadder
             Console.WriteLine("Welcome to snake ladder program.");
             //Constant 
             int START = 0;
-            int POSITION = 0;
+            const int NOPLAY = 1;
+            const int SNAKE = 2;
+            const int LADDER = 3;
+            int position = 0;
             //Dice logic
             Random random = new Random();
             int dice = random.Next(1, 7);
+            int option = random.Next(1, 4);
 
+            switch (option)
+            {
+                case NOPLAY:
+                    position += 0;
+                    break;
+                case SNAKE:
+                    position += dice;
+                    break;
+                case LADDER:
+                    position -= dice;
+                    break;
+            }
         }
     }
 }
